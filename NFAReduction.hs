c70d0497288c27e41dff69a8a4bed740c8ce6ae8
David Xu <dxu2012@gmail.com>
2014-05-23 04:48:04 -0400

    Clean up NFAReduction

--- NFAReduction.hs
+++ NFAReduction.hs
@@ -2,64 +2,71 @@
 
 module NFAReduction where
 
-import FA.DirectState
-import qualified FA.IndirectState as IS
-import FA.Automaton
-import qualified Data.Set as Set
-import Data.Set (Set)
-import qualified Data.MultiMap as MultiMap
-import qualified Data.Map as Map
 import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.MultiMap (MultiMap)
+import qualified Data.MultiMap as MultiMap
+import Data.Set (Set)
+import qualified Data.Set as Set
+import FA.Automaton
+import FA.DirectState (StateType(..), NFAState, DFATransitions)
+import qualified FA.DirectState as DS
+import qualified FA.IndirectState as IS
 
 reduceNFA   :: NFA String -> DFA String
 reduceNFA n = indirectToDirectFA $ GenFA
-              { states = map convertState newStates
-              , startState = convertState $ (equivs "") $ startState n }
+              { states     = map convertState newStates
+              , startState = convertState . (equivs "") $ startState n }
     where newStates = powerlist $ states n
           stateIdMap :: Map (Set Int) Int
-          stateIdMap = Map.fromList $ zip
-                       (map Set.fromList $ map (map stateId) newStates) [1,2..]
-          convertState :: [NFAState String] ->
-                          IS.GenFAState' Int StateType DFATransitions String
-          convertState list =
-              let (sId, sTp, trans) = accumStates list [] NonFinal Map.empty in
-              case Map.lookup (Set.fromList sId) stateIdMap of
-                Nothing -> error "Unknown set of Ids"
-                Just k  -> IS.GenFAState'
-                           { IS.stateId = k
-                           , IS.stateType = sTp
-                           , IS.transitions = Map.filterWithKey
-                                              (\key _ -> not (null key))
-                                              $ Map.map getId trans }
-          getId x = case Map.lookup (Set.fromList x) stateIdMap of
-                      Nothing -> error "Unknown transition"
-                      Just k' -> k'
+          stateIdMap = Map.fromList
+                       $ zip (map (Set.fromList . map DS.stateId) newStates) [1..]
+          convertState     :: [NFAState String]
+                           -> IS.GenFAState' Int StateType DFATransitions String
+          convertState sts =
+            let (sIds, sTp, trans) = processStates (Set.empty, NonFinal, Map.empty) sts
+            in case Map.lookup sIds stateIdMap of
+              Nothing -> error "NFAReduction.reduceNFA: unknown set of IDs"
+              Just k  -> IS.GenFAState'
+                         { IS.stateId     = k
+                         , IS.stateType   = sTp
+                         , IS.transitions = Map.filterWithKey
+                                            (const . not . null)
+                                            $ Map.map targetId trans } -- some finessing can be done here by using a SetMultiMap instead
+          targetId     :: [Int] -> Int
+          targetId ids = case Map.lookup (Set.fromList ids) stateIdMap of
+                           Nothing -> error "Unknown transition"
+                           Just k  -> k
 
-powerlist :: [a] -> [[a]]
+powerlist        :: [a] -> [[a]]
 powerlist []     = [[]]
-powerlist (x:xs) = let pow = powerlist xs in
-                   pow ++ map (\y -> x:y) pow
+powerlist (x:xs) = xss ++ map (x:) xss
+  where xss = powerlist xs
 
-accumStates :: [NFAState String] -> [Int] -> StateType -> Map String [Int]
-            -> ([Int], StateType, Map String [Int])
-accumStates [] a b c               = (a, b, c)
-accumStates (x:xs) ids sType trans =
-    accumStates xs (setAdd (stateId x) ids) (orType (stateType x) sType) $
-                    accumTransitions (MultiMap.toList $ transitions x) trans
-    where orType Final _ = Final
-          orType _ Final = Final
-          orType _ _     = NonFinal
+processStates :: (Set Int, StateType, Map String [Int])
+              -> [NFAState String]
+              -> (Set Int, StateType, Map String [Int])
+processStates = foldr f
+  where f st (ids, tp, trs) = (newIds, newTp, newTrs)
+          where newIds = Set.insert (DS.stateId st) ids
+                newTp  = DS.stateType st `orStateType` tp
+                newTrs = processTransitions trs $ DS.transitions st
 
-setAdd :: Int -> [Int] -> [Int]
-setAdd n' l
-    | not (any (==n') l) = n':l
-    | otherwise          = l
+orStateType         :: StateType -> StateType -> StateType
+orStateType Final _ = Final
+orStateType _ Final = Final
+orStateType _ _     = NonFinal
 
-accumTransitions :: [(String, NFAState String)] -> Map String [Int]
-                 -> Map String [Int]
-accumTransitions [] curmap          = curmap
-accumTransitions ((s, n):xs) curmap =
-  accumTransitions xs $ flip (Map.insert s) curmap $
-                   case Map.lookup s curmap of
-                     Nothing -> [stateId n]
-                     Just l  -> setAdd (stateId n) l
+processTransitions :: Map String [Int]
+                   -> MultiMap String (NFAState String)
+                   -> Map String [Int]
+processTransitions = MultiMap.foldrWithKey f
+  where f s n trs = flip (Map.insert s) trs $
+                    case Map.lookup s trs of
+                      Nothing -> [DS.stateId n]
+                      Just l  -> setAdd (DS.stateId n) l -- here's where a SetMultiMap would be nice
+
+setAdd      :: Eq a => a -> [a] -> [a]
+setAdd x xs
+  | not $ any (== x) xs = x : xs
+  | otherwise           = xs
